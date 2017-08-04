ffad80f5dd913cd5725b72a450fde303bbb86d61
lucabrozzi <brozziluca@imaticaonline.it>
2017-08-04 11:46:33 +0200

    switched to react native axios from axios

--- src/services/http.js
+++ src/services/http.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from 'react-native-axios';
 import utils from '../utils';
 import defaultHeaders from '../sources/defaultHeaders';
 import getFeedByHashtag from '../sources/getFeedByHashtag';
